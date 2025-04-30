03ddd9d807e97549285e7a1b15ee35ed7f42daca
sakshishrivastav-dev <sakushri123@gmail.com>
2025-04-30 16:58:57 +0530

    Update greeting message in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     function show(){
-        return "hello from diksha";
+        return "hello from ritu doidiya";
     }
 }
